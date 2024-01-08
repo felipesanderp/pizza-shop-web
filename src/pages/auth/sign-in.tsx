d9210196efd9b2be48580dfd3470a0caa47a4885
felipesanderp <felipe.panisson@hotmail.com>
2024-01-08 15:03:23 -0300

    toast with sonner and shadcn ui

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -26,6 +27,13 @@ export function SignIn() {
     await new Promise((resolve) => setTimeout(resolve, 2000))
 
     console.log(data)
+
+    toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+      action: {
+        label: 'Reenviar',
+        onClick: () => handleSignIn(data),
+      },
+    })
   }
 
   return (
