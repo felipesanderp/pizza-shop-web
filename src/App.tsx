067398611b0263cdc5a7ac6eefabbb44bc11ff24
felipesanderp <felipe.panisson@hotmail.com>
2024-01-09 11:47:47 -0300

    theme provider and theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from '@/components/theme/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
 
 import { router } from './routes'
@@ -10,9 +11,11 @@ import { router } from './routes'
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-      <Toaster richColors />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
