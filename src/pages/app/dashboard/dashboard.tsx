3d5eb6d490b9579428e8c3fe6e3e3856ec82b65a
felipesanderp <felipe.panisson@hotmail.com>
2024-01-10 11:16:07 -0300

    refactor name

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,7 +4,7 @@ import { DayOrdersAmountCard } from './day-orders-amount-card'
 import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
 import { MonthOrdersAmountCard } from './month-orders-amount-card'
 import { MonthRevenueCard } from './month-revenue-card'
-import { RevenueCharts } from './reveneu-charts'
+import { RevenueCharts } from './revenue-charts'
 
 export function Dashboard() {
   return (
