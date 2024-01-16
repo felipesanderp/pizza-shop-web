af939ff08114048f360b29b75f08baa96c679276
felipesanderp <felipe.panisson@hotmail.com>
2024-01-16 10:42:28 -0300

    authnticate fn with react query mutation

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import path from "node:path"
-import { defineConfig } from 'vite'
+import path from 'node:path'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174,
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
